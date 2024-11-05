06e48794c4c1168ee9a2bc91b1cd5ce242fb3465
vraj patel <vraj.patel@primedemo.in>
2024-11-05 23:05:15 +0530

    change css for small devices

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ const App = () => {
 
   return (
     <div className="h-screen w-full bg-gray-400">
-      <div className="w-4/5 h-3/4 bg-indigo-500 p-4 top-1/2 left-1/2 absolute transform -translate-x-1/2 -translate-y-1/2 rounded-xl">
+      <div className="md:w-4/5 md:h-3/4 h-screen w-screen bg-indigo-500 p-4 top-1/2 left-1/2 absolute transform -translate-x-1/2 -translate-y-1/2 rounded-xl">
         <div className="h-full w-full bg-indigo-200 rounded-xl p-2 flex flex-col gap-2">
           <div className="bg-indigo-500 md:p-5 p-3 rounded-xl">
             <p className="text-white font-bold text-center md:text-3xl text-xl">
@@ -79,7 +79,7 @@ const App = () => {
                 Copy
               </button>
             </div>
-            <div className="flex flex-row mx-auto w-3/4 ">
+            <div className="flex flex-row mx-auto md:w-3/4 w-full ">
               <div className="bg-indigo-300 md:p-5 p-3 md:px-7 rounded-xl m-1">
                 <p className="text-white font-extrabold  text-center text-lg items-center">
                   {passwordLength}
@@ -97,7 +97,7 @@ const App = () => {
                 />
               </div>
             </div>
-            <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 w-11/12 mx-auto ">
+            <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 md:w-11/12 w-full mx-auto ">
               <div className="bg-indigo-300 md:p-5 p-2 rounded-xl m-1 flex flex-row items-center w-3/4 mx-auto gap-2">
                 <input type="checkbox" id="uppercase" onChange={(e) => setUpperAllow(e.target.checked)}  checked={upperAllow} className="w-1/4 h-[13px] md:h-[25px]  cursor-pointer" />
                 <p className="text-white font-extrabold text-center flex-1 w-full text-md md:text-xl" >
